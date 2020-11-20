ac9fe8046e58f58db807380f8b2f33ebce1e65f7
chaitu6022 <kkc4al@gmail.com>
2020-11-20 22:09:13 +0000

    updated files and configuration

---
+++ src/main/sqlscripts/changelog1.1.sql
@@ -0,0 +1,8 @@
+
+create TABLE sqldemo.candidate(
+    candidate_id character varying(255) NOT NULL,
+    candidate_first_name character varying(255) NOT NULL,
+    candidate_middle_name character varying(255) NOT NULL,
+    candidate_last_name character varying(255) NOT NULL
+);
+
